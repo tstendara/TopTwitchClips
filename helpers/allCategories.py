0ec6d25239619a8405b096a91d029ee59214a193
Tyler <tstendara@gmail.com>
2020-07-03 02:29:06 -0500

    able to create video for multiple games

--- helpers/allCategories.py
+++ helpers/allCategories.py
@@ -1,5 +1,8 @@
 game_dict = {
 'Overwatch': {'approved_channels': ['supertf','m0xyy', 'req__ow', 'chipsa'
 , 'emongg', 'necros', 'jay3', 'sleepy', 'a_seagull', 'surefour', 'jake_ow', 
-'agilities', 'ml7support', 'sinatraa', 'Yeatle', 'space']
-}}
+'agilities', 'ml7support', 'sinatraa', 'yeatle', 'space']},
+
+'Fortnite': {'approved_channels': ['Tfue', 'mrfaildruid', 'jennysmiles',
+'Crayator', 'benjyfishy', 'BDM_RAT', 'SypherPK', 'Evai', 'TabzG']}
+}
