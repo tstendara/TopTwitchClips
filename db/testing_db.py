71e540ce79868218d4ef165720f4cf39feab8084
Tyler <tstendara@gmail.com>
2020-07-22 22:13:05 -0500

    new testing for resetting password

--- db/testing_db.py
+++ db/testing_db.py
@@ -1,11 +1,12 @@
-from unittest import TestCase
+import unittest
 from db_config import MongoDb
-config = MongoDb('email@example.com', '12345', 'twitch_user')
+
+config = MongoDb('email@example.com', 'newpassword', 'twitch_user')
 config.initializingConnection()
 
-class testingDb(TestCase):
+class testingDb(unittest.TestCase):
     # test connection of mongodb, signup, login, and erorr checking with sign up 
-    # and signup with same email
+    # and signup with same email, and resetting password
 
     def test_conn(self):
         conn = config.establishingConn()
@@ -23,7 +24,7 @@ class testingDb(TestCase):
 
     def test_delete_account(self):
         config.deleteUser()
-        config.username, config.password, config.twitchUsername = 'email@example.com', '12345', 'twitch_user'
+        config.username, config.password, config.twitchUsername = 'email@example.com', 'newPassword', 'twitch_user'
         successfulLogin = config.login()
         self.assertEqual(successfulLogin, None)
 
@@ -32,5 +33,19 @@ class testingDb(TestCase):
         successfulLogin = config.login()
         self.assertEqual(successfulLogin, True)
 
+    def test_changing_password(self):
+        temporaryPassword = config.sendingRecoveryPassword()
+        if temporaryPassword:
+            email = config.recoveryEmail(temporaryPassword)
+            if email:
+                config.resettingPassword(email, 'newpassword')
+                self.assertEqual(True, True)
+            else:
+                self.assertEqual(print(email), 'Didnt get back email needed to reset password | There was a problem finding the temporary password in makingVideos.recoveryAccount')
+        else:
+            self.assertEqual(print(temporaryPassword), 'Didnt get back temporary password | check to see if testing env var exists and is = True and make sure that email exits already')
+        
 
+if __name__ == "__main__":
+    unittest.main()
 
